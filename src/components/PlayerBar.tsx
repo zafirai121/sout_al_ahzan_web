e7c70d5066cc0b938ad1cdcd8e69bcbaaf22d243
Developer <dev@sout-al-ahzan.com>
2026-07-05 08:23:12 +0300

    fix: move volume slider before icon for RTL layout

--- src/components/PlayerBar.tsx
+++ src/components/PlayerBar.tsx
@@ -480,13 +480,6 @@ export default function PlayerBar() {
           </div>
           
           <div style={{ display: 'flex', alignItems: 'center', gap: '8px', width: '93px' }}>
-            <button className="sp-btn" onClick={() => setIsMuted(!isMuted)}>
-              {isMuted || volume === 0 ? (
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M13.86 5.47a.75.75 0 00-1.06 1.06l1.47 1.47-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L16.39 8l1.47-1.47a.75.75 0 00-1.06-1.06l-1.47 1.47-1.47-1.47zM.5 5.5v5h3l5 4.5v-14l-5 4.5h-3z"/></svg>
-              ) : (
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/></svg>
-              )}
-            </button>
             <div className="sp-slider-wrapper">
               <div className="sp-slider-track">
                 <div className="sp-slider-fill" style={{ width: `${volumePercent}%` }}></div>
@@ -502,6 +495,13 @@ export default function PlayerBar() {
                 className="sp-range"
               />
             </div>
+            <button className="sp-btn" onClick={() => setIsMuted(!isMuted)}>
+              {isMuted || volume === 0 ? (
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M13.86 5.47a.75.75 0 00-1.06 1.06l1.47 1.47-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L16.39 8l1.47-1.47a.75.75 0 00-1.06-1.06l-1.47 1.47-1.47-1.47zM.5 5.5v5h3l5 4.5v-14l-5 4.5h-3z"/></svg>
+              ) : (
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/></svg>
+              )}
+            </button>
           </div>
         </div>
       </footer>
